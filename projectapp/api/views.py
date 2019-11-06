458987401828efe81742b44e55f311931d83e74f
imran1234567 <sk2read@gmail.com>
2019-11-06 18:19:05 +0530

    fuzzy search implemented

--- projectapp/api/views.py
+++ projectapp/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import generics
 from projectapp.models import Search
 from .serializers import SearchModelSerializers
@@ -5,5 +6,12 @@ from .serializers import SearchModelSerializers
 class SearchListAPIView(generics.ListAPIView):
     serializer_class = SearchModelSerializers
 
-    def get_queryset(self):
-        return Search.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        qs = Search.objects.all()
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                    Q(name__icontains=query) |
+                    Q(custom_number__icontains=query)
+                    )
+        return qs
