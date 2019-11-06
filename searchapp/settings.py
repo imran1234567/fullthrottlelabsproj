5367e5e7380b861a9cb6aadb36f32048ad3dfb59
imran1234567 <sk2read@gmail.com>
2019-11-06 12:29:25 +0530

    test

--- searchapp/settings.py
+++ searchapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tuq&jvw*ohfe)g(d46!qxn+6x0d)jcrq6)-m)0hm711m$90l-!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fullthrottlelabsproj.herokuapp.com']
+ALLOWED_HOSTS = ['fullthrottlelabsproj.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -133,3 +133,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
